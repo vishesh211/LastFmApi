abebba00da965321c7d1d4e40046558aba594953
vishesh <visheshtiwari211@gmail.com>
2023-03-04 22:58:56 +0530

    feat: created LastFmInstance.kt and deleted LastFmModule.kt

---
+++ app/src/main/java/com/example/musicwiki/LastFmInstance.kt
@@ -0,0 +1,15 @@
+package com.example.musicwiki
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object LastFmInstance {
+    val lastFmApi: LastFmApi by lazy {
+        Retrofit
+            .Builder()
+            .baseUrl(LastFmApi.URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(LastFmApi::class.java)
+    }
+}
