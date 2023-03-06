e1c2780fc63b8f6dfaa930285a26f355bb665a1a
vishesh <visheshtiwari211@gmail.com>
2023-03-06 18:34:27 +0530

    feat: added GenreInfo in dataclasses

---
+++ app/src/main/java/com/example/musicwiki/dataclasses/GenreInfo.kt
@@ -0,0 +1,5 @@
+package com.example.musicwiki.dataclasses
+
+data class GenreInfo(
+    val tag: TagXXX
+)
---
+++ app/src/main/java/com/example/musicwiki/dataclasses/TagXXX.kt
@@ -0,0 +1,8 @@
+package com.example.musicwiki.dataclasses
+
+data class TagXXX(
+    val name: String,
+    val reach: Int,
+    val total: Int,
+    val wiki: WikiX
+)
