849ec2c7860d4cdb88fbb4e0a9a187c37b4fc1a9
vishesh211 <visheshtiwari211@gmail.com>
2023-04-09 21:51:11 +0530

    fix: refactoring

--- app/src/main/java/com/example/lastfmapi/ui/ArtistDetailActivity.kt
+++ app/src/main/java/com/example/lastfmapi/ui/ArtistDetailActivity.kt
@@ -69,7 +69,7 @@ class ArtistDetailActivity : AppCompatActivity() {
         }
     }
 
-    fun getFormattedNumber(count: Long): String {
+    private fun getFormattedNumber(count: Long): String {
         if (count < 1000) return "" + count
         val exp = (ln(count.toDouble()) / ln(1000.0)).toInt()
         return String.format("%.1f %c", count / 1000.0.pow(exp.toDouble()), "kMGTPE"[exp - 1])
